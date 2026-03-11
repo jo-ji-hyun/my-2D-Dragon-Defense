50f4a7b0852c4f100c33a9032414340fe6745228
jo-ji-hyun <newterr@naver.com>
2026-03-11 21:04:24 +0900

    [Feat] 엑셀 데이터를 So로 변형
    테스트 유닛 추가

---
+++ Assets/05.Scripts/Data/GameData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[ExcelAsset(AssetPath ="06.ScriptableObject")]
+public class GameData : ScriptableObject
+{
+	public List<UnitData> Unit; 
+	//public List<EntityType> Enemy; 
+}
---
+++ Assets/05.Scripts/Data/UnitData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+[Serializable]
+public class UnitData
+{
+    public int Id;
+    public string Name;
+    public int Hp;
+    public int Atk;
+    public int AtkDelay;
+    public int Price;
+    public string Icon;
+}

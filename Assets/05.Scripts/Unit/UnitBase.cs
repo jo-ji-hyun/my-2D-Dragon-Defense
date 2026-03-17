f99f6dd6bbe9ee9084d81692ffc1e9473675419a
jo-ji-hyun <newterr@naver.com>
2026-03-17 20:32:04 +0900

    [Feat] Rigidbody를 이용한 유닛 움직임 구현

---
+++ Assets/05.Scripts/Unit/UnitBase.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class UnitBase : MonoBehaviour
+{
+    [SerializeField] private new Rigidbody2D rigidbody2D;
+    [SerializeField] private float move_Speed = 2.0f;
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.velocity = new Vector2(move_Speed, rigidbody2D.velocity.y);
+    }
+}
